632d62c6be6ac7df0720c6b5c4fb2cac3acb91b6
Alex Messersmith <alex.knifesmith@gmail.com>
2019-12-07 18:22:17 -0600

    Create basic OpModes

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HardwareOldBot.java
@@ -0,0 +1,107 @@
+/* Copyright (c) 2017 FIRST. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted (subject to the limitations in the disclaimer below) provided that
+ * the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice, this list
+ * of conditions and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright notice, this
+ * list of conditions and the following disclaimer in the documentation and/or
+ * other materials provided with the distribution.
+ *
+ * Neither the name of FIRST nor the names of its contributors may be used to endorse or
+ * promote products derived from this software without specific prior written permission.
+ *
+ * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
+ * LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+/**
+ * Defines the hardware for our old robot
+ * (the one with four straight wheels and front omni-wheels)
+ * <p>
+ * Motor channel:  Left  drive motor:        "leftDrive"
+ * Motor channel:  Right drive motor:        "rightDrive"
+ * Motor channel:  Arm drive motor:          "armMotor"
+ * Servo channel:  Servo to open left claw:  "leftArmServo"
+ * Servo channel:  Servo to open right claw: "rightArmServo"
+ */
+public class HardwareOldBot {
+    // Declare motor names
+    private String LEFT_MOTOR_NAME = "leftDrive";
+    private String RIGHT_MOTOR_NAME = "rightDrive";
+    private String ARM_MOTOR_NAME = "armMotor";
+    private String LEFT_ARM_SERVO_NAME = "leftArmServo";
+    private String RIGHT_ARM_SERVO_NAME = "rightArmServo";
+
+    // Declare hardware
+    private DcMotor leftDrive = null;
+    private DcMotor rightDrive = null;
+    private DcMotor armMotor = null;
+    private Servo leftArmServo = null;
+    private Servo rightArmServo = null;
+
+    /* local OpMode members. */
+    HardwareMap hardwareMap = null;
+    private ElapsedTime runtime = new ElapsedTime();
+
+    /* Constructor */
+    public HardwareOldBot() {
+
+    }
+
+    /* Initialize standard Hardware interfaces */
+    public void init(HardwareMap mHardwareMap) {
+        // Save reference to Hardware map
+        hardwareMap = mHardwareMap;
+
+        // Define and Initialize Motors
+        leftDrive = hardwareMap.get(DcMotor.class, LEFT_MOTOR_NAME);
+        rightDrive = hardwareMap.get(DcMotor.class, RIGHT_MOTOR_NAME);
+        armMotor = hardwareMap.get(DcMotor.class, ARM_MOTOR_NAME);
+
+        armMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        leftDrive.setDirection(DcMotor.Direction.REVERSE); // Set to REVERSE if using AndyMark motors
+        rightDrive.setDirection(DcMotor.Direction.FORWARD);// Set to FORWARD if using AndyMark motors
+
+        // Set all motors to zero power
+        leftDrive.setPower(0);
+        rightDrive.setPower(0);
+        armMotor.setPower(0);
+
+        // Set all motors to run without encoders.
+        // May want to use RUN_USING_ENCODERS if encoders are installed.
+        leftDrive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        rightDrive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        armMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+        // Define and initialize ALL installed servos.
+        leftArmServo = hardwareMap.get(Servo.class, LEFT_ARM_SERVO_NAME);
+        rightArmServo = hardwareMap.get(Servo.class, RIGHT_ARM_SERVO_NAME);
+
+        // Set servo directions
+        leftArmServo.setDirection(Servo.Direction.FORWARD);
+        rightArmServo.setDirection(Servo.Direction.REVERSE);
+    }
+}
+
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LessBasicAuto.java
@@ -0,0 +1,148 @@
+/* Copyright (c) 2017 FIRST. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted (subject to the limitations in the disclaimer below) provided that
+ * the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice, this list
+ * of conditions and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright notice, this
+ * list of conditions and the following disclaimer in the documentation and/or
+ * other materials provided with the distribution.
+ *
+ * Neither the name of FIRST nor the names of its contributors may be used to endorse or
+ * promote products derived from this software without specific prior written permission.
+ *
+ * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
+ * LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+/**
+ * This file illustrates the concept of driving a path based on encoder counts.
+ * It uses the common Pushbot hardware class to define the drive on the robot.
+ * The code is structured as a LinearOpMode
+ * <p>
+ * The code REQUIRES that you DO have encoders on the wheels,
+ * otherwise you would use: PushbotAutoDriveByTime;
+ * <p>
+ * This code ALSO requires that the drive Motors have been configured such that a positive
+ * power command moves them forwards, and causes the encoders to count UP.
+ * <p>
+ * The desired path in this example is:
+ * - Drive forward for 48 inches
+ * - Spin right for 12 Inches
+ * - Drive Backwards for 24 inches
+ * - Stop and close the claw.
+ * <p>
+ * The code is written using a method called: encoderDrive(speed, leftInches, rightInches, timeoutS)
+ * that performs the actual movement.
+ * This methods assumes that each movement is relative to the last stopping place.
+ * There are other ways to perform encoder based moves, but this method is probably the simplest.
+ * This code uses the RUN_TO_POSITION mode to enable the Motor controllers to generate the run profile
+ * <p>
+ * Use Android Studios to Copy this Class, and Paste it into your team's code folder with a new name.
+ * Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode list
+ */
+
+@Autonomous(name = "Actual Auto", group = "Auto")
+//@Disabled
+public class LessBasicAuto extends LinearOpMode {
+    /* Declare OpMode members. */
+    private ElapsedTime runtime = new ElapsedTime();
+    DcMotor leftDrive = null;
+    DcMotor rightDrive = null;
+    DcMotor armMotor = null;
+    private Servo leftArmServo = null;
+    private Servo rightArmServo = null;
+
+    @Override
+    public void runOpMode() {
+        leftDrive = hardwareMap.get(DcMotor.class, "leftDrive");
+        rightDrive = hardwareMap.get(DcMotor.class, "rightDrive");
+        armMotor = hardwareMap.get(DcMotor.class, "armMotor");
+        leftArmServo = hardwareMap.get(Servo.class, "leftArmServo");
+        rightArmServo = hardwareMap.get(Servo.class, "rightArmServo");
+
+        // Send telemetry message to signify robot waiting;
+        telemetry.addData("Status", "Resetting Encoders");
+        telemetry.update();
+
+        leftDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        rightDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        armMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+        leftDrive.setDirection(DcMotor.Direction.REVERSE);
+        rightDrive.setDirection(DcMotor.Direction.FORWARD);
+        armMotor.setDirection(DcMotor.Direction.FORWARD);
+
+        leftArmServo.setDirection(Servo.Direction.FORWARD);
+        rightArmServo.setDirection(Servo.Direction.REVERSE);
+
+        // Wait for the game to start (driver presses PLAY)
+        waitForStart();
+
+        // Raise the arm first
+        armMotor.setTargetPosition(840);
+        armMotor.setPower(0.5d);
+
+        while(armMotor.isBusy()) {
+            telemetry.addData("Arm", String.valueOf(armMotor.getCurrentPosition()));
+        }
+
+        setServos(0.5d);
+        sleep(500);
+        armMotor.setPower(0d);
+
+        // Run the drive motors for one rotation
+        // AndyMark encoders have 1680 counts per revolution
+        leftDrive.setTargetPosition(1680);
+        rightDrive.setTargetPosition(1680);
+
+        leftDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        rightDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+        leftDrive.setPower(0.4d);
+        rightDrive.setPower(0.4d);
+
+        while(leftDrive.isBusy() && rightDrive.isBusy()) {
+            telemetry.addData("Drive", "left " + leftDrive.getCurrentPosition() + " right " +
+                    rightDrive.getCurrentPosition());
+        }
+
+        leftDrive.setPower(0d);
+        rightDrive.setPower(0d);
+
+        leftDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        rightDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+        telemetry.addData("Path", "Complete");
+        telemetry.update();
+    }
+
+    /*
+     * Set BOTH servos at the same time
+     */
+    public void setServos(double position) {
+        leftArmServo.setPosition(position);
+        rightArmServo.setPosition(position);
+    }
+
+}
